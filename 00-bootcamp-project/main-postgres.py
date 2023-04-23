3500fb51e4ef9c413e45c6404c55aea6abe6ab9f
Kirana Th <36392004+KiranaTh@users.noreply.github.com>
2023-04-23 10:06:51 +0000

    fix homework-01

--- 00-bootcamp-project/main-postgres.py
+++ 00-bootcamp-project/main-postgres.py
@@ -86,6 +86,19 @@ with open(f"{DATA_FOLDER}/{table}.csv", "w") as f:
     for each in results:
         writer.writerow(each)
 
+table = "promos"
+header = ["promo_id","discount","status"]
+with open(f"{DATA_FOLDER}/{table}.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+    query = f"select * from {table}"
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+    for each in results:
+        writer.writerow(each)
+
 
 table = "users"
 header = ["user_id","first_name","last_name","email","phone_number","created_at","updated_at","address_id"]
