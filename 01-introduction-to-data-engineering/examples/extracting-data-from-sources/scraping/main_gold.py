8dc19fc32ad203250a491e3a76bf6cd3aa0cfcb3
Kirana Th <36392004+KiranaTh@users.noreply.github.com>
2023-05-07 10:30:43 +0000

    workshop-day3

--- 01-introduction-to-data-engineering/examples/extracting-data-from-sources/scraping/main_gold.py
+++ 01-introduction-to-data-engineering/examples/extracting-data-from-sources/scraping/main_gold.py
@@ -1,7 +1,13 @@
 import csv
+import json
+import os
+import sys
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from google.api_core import exceptions
+from google.cloud import storage
+from google.oauth2 import service_account
 
 
 URL = "https://ทองคําราคา.com/"
@@ -27,7 +33,37 @@ class MySpider(scrapy.Spider):
             # print(row.xpath("td//text()").extract())
 
         # Write to CSV
-        # YOUR CODE HERE
+        with open("price.csv", "w") as f:
+            writer = csv.writer(f)
+            header = rows
+            writer.writerow(header)
+
+            for row in rows:
+                writer.writerow(row.css("td::text").extract())
+
+        # The ID of your GCS bucket
+        bucket_name = "jill-100005"
+        # The path to your file to upload
+        source_file_name = "price.csv"
+        # The ID of your GCS object
+        destination_blob_name = "gold_price/2023-05-07/price.csv"
+
+        keyfile = os.environ.get("KEYFILE_PATH")
+        service_account_info = json.load(open(keyfile))
+        credentials = service_account.Credentials.from_service_account_info(service_account_info)
+        project_id = "liquid-optics-384501"
+
+        storage_client = storage.Client(
+            project=project_id,
+            credentials=credentials,
+        )
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+
+        print(
+            f"File {source_file_name} uploaded to {destination_blob_name}."
+        )
 
 
 if __name__ == "__main__":
